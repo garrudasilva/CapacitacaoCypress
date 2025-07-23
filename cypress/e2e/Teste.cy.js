50905f839f882ffdcc04248824b639d41ed7ada9
Felix De Freitas <dfelix@minsait.com>
2025-07-23 17:53:03 -0300

    Adicionando mais comandos e iniciando as validações

--- cypress/e2e/Teste.cy.js
+++ cypress/e2e/Teste.cy.js
@@ -1,11 +1,27 @@
+/// <reference types='cypress' />
+
+
 describe('Teste - Formulário', () => {
   it('Preencher campos com sucesso', () => {
     cy.visit('https://testautomationpractice.blogspot.com/')
     cy.get('#name').type('Teste')
     cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com')
     cy.get('#phone').type('83988744525')
-
-
+    cy.get('input[value="female"]').click()
+    cy.get('input[type="checkbox"]').check('sunday')
+    cy.get('#country').select('Canada')
+    cy.get('#colors').select('Red')
+    cy.get('#datepicker').click()
+    cy.get('a[data-date="24"]').click()
+    cy.get('#datepicker').should('have.value','07/24/2025')
+    cy.get('#singleFileInput').selectFile('cypress\\fixtures\\imgTest.png')
+    cy.get('#singleFileInput').should('have.value','C:\\fakepath\\cypress\\fixtures\\imgTest.png')
+    
+  })
+  it.only('Validar botão Enter', () =>{
+    cy.visit('https://testautomationpractice.blogspot.com/')
+    cy.get('button[name="start"]').click()
+    cy.get('button[name="stop"]').should('be.visible')
   })
 
 })
